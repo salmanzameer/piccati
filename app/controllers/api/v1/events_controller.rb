4a92fde03440ff5001ad14574c6c4c7019e8edec
bilal_protonlabs <bilalashraf141@yahoo.com>
2016-02-24 14:40:48 +0500

    like working

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,113 +4,117 @@ class Api::V1::EventsController < ApplicationController
     @client = Client.find(params[:client_id])
     @event  = @client.events
     render :json => { status: 'Ok' ,events: @event,
-    client: { id: @client.id ,name:  @client.first_name, username: @client.user_name, token: @client.authentication_token } } 
-  end
+      client: { id: @client.id ,name:  @client.first_name, username: @client.user_name, token: @client.authentication_token } } 
+    end
 
-  def show
+    def show
 
-    if current_photographer.present? 
-      @client = Client.find_by_id(params[:client_id]) 
-      @event  = Event.find_by_id(params[:id])
-    else
-      client_match_token
-      @event  = @client.events.find_by_id(params[:id])
-      if @client.present?
-
-        render :json => { status: 'Ok' ,event: @client.events.find_by_id(params[:id]) }
+      if current_photographer.present? 
+        @client = Client.find_by_id(params[:client_id]) 
+        @event  = Event.find_by_id(params[:id])
       else
-        render :json => { status: 'Error'}
+        client_match_token
+        @event  = @client.events.find_by_id(params[:id])
+        if @client.present?
+
+          render :json => { status: 'Ok' ,event: @client.events.find_by_id(params[:id]) }
+        else
+          render :json => { status: 'Error'}
+        end
       end
     end
-  end
 
-  def new
-    @client =current_photographer.clients.find_by_id(params[:client_id]) 
-    @event = current_photographer.clients.find_by_id(params[:client_id]).events.new
-  end
-
-  def create
-    @event = current_photographer.clients.find_by_id(params[:client_id]).events.new(eventparams)
-    if
-     @event.save
-     @event.images.create(params.require(:event).permit(:image)) 
-     redirect_to  api_v1_photographer_client_path(current_photographer,params[:client_id])  
-   else
-    flash[:notice] ='Error'
-    render('new')
-  end
-end
-
-def eventparams
-   params.require(:event).permit(:name,:location,:bridal,:groom)
-end
-
-def upload_images
-      #@client = Client.find_by_id(params[:client_id])
-      param = { "image" => params[:file] }
-      @image = current_photographer.clients.find_by_id(params[:client_id]).events.find_by_id(params[:id]).images.create(param)
-
+    def new
+      @client =current_photographer.clients.find_by_id(params[:client_id]) 
+      @event = current_photographer.clients.find_by_id(params[:client_id]).events.new
     end
 
-    def showevents
-      @event =current_photographer.events.all
-             
-    end
-
-    def all_images
-      if current_photographer.present?
-       @client = Client.find_by_id(params[:client_id])
-       @event  = Event.find_by_id(params[:id])
-       @image  = current_photographer.clients.find_by_id(params[:client_id]).events.find_by_id(params[:id]).images.all    
+    def create
+      @event = current_photographer.clients.find_by_id(params[:client_id]).events.new(eventparams)
+      if
+       @event.save
+       @event.images.create(params.require(:event).permit(:image)) 
+       redirect_to  api_v1_photographer_client_path(current_photographer,params[:client_id])  
      else
-      client_match_token
-      if @client.present?  
-        @image = @client.events.find_by_id(params[:event_id]).images.all.each do |img|
-        @i = request.base_url + img.image_url
-        img.update_attributes(url: @i) 
-      end
-        render :json => {status: "Ok" ,images: @image.as_json(:only => [:id, :event_id, :url]) }  
-      else
-        render :json => { status: "error" }  
-       end
-     end
-   end  
-
-   def like
-  
-    if current_photographer.present?
-     @image = Image.find_by_id(params[:image_id])
-   else
-    client_match_token
-    @like = Image.find_by_id(params[:id]).update_attributes(is_liked: params[:is_liked])
-     render :json => { status: "Ok" ,like: @like} 
+      flash[:notice] ='Error'
+      render('new')
     end
-   end
+  end
 
-  def image
-#binding.pry     
-    if current_photographer.present?
-       @client =  Client.find_by_id(params[:client_id])
-       @event  =  Event.find_by_id(params[:event_id])
-       @image  =  Image.find_by_id(params[:image_id])
-        
+  def eventparams
+   params.require(:event).permit(:name,:location,:bridal,:groom)
+ end
 
-    else
-      client_match_token
-      
-        @image = Image.find_by_id(params[:id])
-    
-      if @client.present?
+ def upload_images
+  param = { "image" => params[:file] }
+  @image = current_photographer.clients.find_by_id(params[:client_id]).events.find_by_id(params[:id]).images.create(param)
 
-       render :json =>  { status: "Ok", image: @image.as_json(:only =>[:id, :name, :event_id, :url] )  }
+end
 
-      else
-        render :json => { status: 'error'}
-      end
+def showevents
+  @event =current_photographer.events.all
+
+end
+
+def all_images
+  if current_photographer.present?
+   @client = Client.find_by_id(params[:client_id])
+   @event  = Event.find_by_id(params[:id])
+   @image  = current_photographer.clients.find_by_id(params[:client_id]).events.find_by_id(params[:id]).images.all    
+ else
+  client_match_token
+  if @client.present?  
+    @image = @client.events.find_by_id(params[:event_id]).images.all.each do |img|
+      @i = request.base_url + img.image_url
+      img.update_attributes(url: @i) 
     end
-    
+    render :json => {status: "Ok" ,images: @image.as_json(:only => [:id, :event_id, :url]) }  
+  else
+    render :json => { status: "error" }  
+  end
+end
+end  
 
-    
+def like
+#binding.pry
+  if current_photographer.present?
+   @image = Image.find_by_id(params[:image_id])
+ else
+  client_match_token
+  if @client.present?
+  @like = Image.find_by_id(params[:id]).update_attributes(is_liked: params[:is_liked])
+  @like = Image.find_by_id(params[:id])
+  render :json => { status: "Ok" ,like: @like.is_liked}
+  else
+     render :json => {status: "Error"}
+  end 
+end
+end
+
+def image
+
+  if current_photographer.present?
+   @client =  Client.find_by_id(params[:client_id])
+   @event  =  Event.find_by_id(params[:event_id])
+   @image  =  Image.find_by_id(params[:image_id])
+
+
+ else
+  client_match_token
+
+  @image = Image.find_by_id(params[:id])
+
+  if @client.present?
+
+   render :json =>  { status: "Ok", image: @image.as_json(:only =>[:id, :name, :event_id, :url] )  }
+
+ else
+  render :json => { status: 'error'}
+end
+end
+
+
+
 
 end
 end
