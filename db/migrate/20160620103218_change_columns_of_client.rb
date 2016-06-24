29961b0bd1efc5ec673ed4f960bf8978547e07af
Ammar Nadeem <ammar@protonlabs.com>
2016-06-24 18:33:35 +0500

    resolved heroku migration error

--- db/migrate/20160620103218_change_columns_of_client.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnsOfClient < ActiveRecord::Migration
-  def change
-  	remove_column :clients, :email
-    remove_column :clients, :password 
-  end
-end
--- db/migrate/20160620105235_drop_clients.rb
+++
@@ -1,5 +0,0 @@
-class DropClients < ActiveRecord::Migration
-  def change
-  	drop_table :clients
-  end
-end
