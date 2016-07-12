53c590cd1756f103b5adcddabc3863d270005796
Ammar Nadeem <ammar@protonlabs.com>
2016-07-12 12:13:21 +0500

    resolved error for api calls

--- app/models/image.rb
+++ app/models/image.rb
@@ -24,7 +24,7 @@ class Image < ActiveRecord::Base
 
 
   def watermark_url
-    Photographer.current_photographer.watermark_logo.url
+    Photographer.current_photographer.watermark_logo.url if Photographer.current_photographer.present?
   end
 
 	def image_url
