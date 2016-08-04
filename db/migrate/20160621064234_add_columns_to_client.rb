2a91ec63c975f3f286775cbd8438077734cebaad
Salman Zameer <salman.railstech@gmail.com>
2016-08-04 12:13:23 +0500

    remove some mgrn

--- db/migrate/20160621064234_add_columns_to_client.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToClient < ActiveRecord::Migration
-  def change
-  	add_column :clients, :website, :string
-  	add_column :clients, :contnumber, :string
-  	add_column :clients, :title, :string
-  end
-end
