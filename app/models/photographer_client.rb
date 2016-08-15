cb618665723be2bbfba780cb9a95f9d6cbe2cecf
Naveed Tariq <naved.tariq@gmail.com>
2016-08-15 21:57:48 +0500

    removing errors

--- app/models/photographer_client.rb
+++ app/models/photographer_client.rb
@@ -4,7 +4,7 @@ class PhotographerClient < ActiveRecord::Base
 	belongs_to :client
 
   validates :package, presence: true, on: :update
-  validates :total, presence: true, on: :update
+  validates :total, 	presence: true, on: :update
   validates :balance, presence: true, on: :update
   validates :advance, presence: true, on: :update
 end
