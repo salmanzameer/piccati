6f589ce0da74e7124a61aba66d858e5c863dbdbc
Naveed Tariq <naved.tariq@gmail.com>
2016-08-24 14:37:37 +0500

    updated url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.action_mailer.default_url_options = { :host => 'app.piccati.com' }
+  config.action_mailer.default_url_options = { :host => 'http://app.piccati.com' }
   config.i18n.fallbacks = true
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
