e86a775802eef72bb0dc8082d0a7361d42399600
Naveed Tariq <naved.tariq@gmail.com>
2016-08-25 23:21:13 +0500

    setting domian name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'piccati.com',
     :enable_starttls_auto => true
   }
 
