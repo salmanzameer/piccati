adfafae2f1ee06567aaaedf551491e0be9b48ae9
Ammar Nadeem <ammar@protonlabs.com>
2016-08-29 20:17:33 +0500

    updated email text

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -2,11 +2,13 @@ class InvitationMailer < ApplicationMailer
 	
 	def client_invitation(current_photographer, email)
 		@photographer = current_photographer
+		@client = Client.find_by_email(email)
 		mail to: "#{email}", subject: "Invitation"
 	end
 
-	def client_acknowledge(current_photographer, email)
+	def client_acknowledge(current_photographer, client)
 		@photographer = current_photographer
-		mail to: "#{email}", subject: "Invitation"
+		@client = client
+		mail to: "#{@client.email}", subject: "Add as a C
 	end
 end
