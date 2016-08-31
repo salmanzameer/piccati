914acaf8a5b6f56bce1fb0eba78a514757c0b7e8
Salman Zameer <salman.railstech@gmail.com>
2016-08-31 20:57:12 +0500

    added condition not to show unconfirmed photographer

--- app/api/v1/snapper/general_data.rb
+++ app/api/v1/snapper/general_data.rb
@@ -5,7 +5,7 @@ module Snapper
       optional :page,                   type: Integer
     end
     get :photographers, rabl: "v1/snapper/photographers_index" do
-	    @photographer = Photographer.all.paginate( page: params[:page], per_page: 6 ).order("created_at ASC")
+	    @photographer = Photographer.where("confirmed_at IS NOT NULL").paginate( page: params[:page], per_page: 6 ).order("created_at ASC")
 	    unless @photographer
 	      throw :error, status: 404, message: "Photographer not found!"
 	    end
