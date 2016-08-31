3052aef173a94530ac059b04562fea69cff12a54
Salman Zameer <salman.railstech@gmail.com>
2016-08-31 17:47:44 +0500

    removed packages

--- db/migrate/20160830131139_create_packages.rb
+++ db/migrate/20160830131139_create_packages.rb
@@ -1,14 +1,5 @@
 class CreatePackages < ActiveRecord::Migration
   def change
-    create_table :packages do |t|
-      t.string :name
-      t.integer :picture_dslr_count
-      t.integer :video_dslr_count
-      t.integer :album_leaves
-      t.float :other_city_charges
-      t.integer :photographer_id
-
-      t.timestamps null: false
-    end
+    drop_table :packages
   end
 end
