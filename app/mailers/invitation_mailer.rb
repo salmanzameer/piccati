bb896b87df793483dda27373af56e848f8d79ffe
Salman Zameer <salman.railstech@gmail.com>
2016-09-01 18:36:27 +0500

    added new bg

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -9,6 +9,6 @@ class InvitationMailer < ApplicationMailer
 	def client_acknowledge(current_photographer, client)
 		@photographer = current_photographer
 		@client = client
-		mail to: "#{@client.email}", subject: "#{current_photographer.fullname} added you as client on Piccati."
+		mail to: "#{@client.email}", subject: "Photographer added you as a client"
 	end
 end
