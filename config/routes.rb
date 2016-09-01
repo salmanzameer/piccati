a271fa42ffbaf3734d517a18e3b5c4ec27fee734
talha <quiet.talha@gmail.com>
2016-09-01 11:32:35 +0500

    Route bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'admins/new'
-  #get 'admins/dashboard'
+  get 'admins/dashboard'
   get 'admins/show_all_albums'
   get 'admins/album_images'
   get 'admins/update_all_images'
