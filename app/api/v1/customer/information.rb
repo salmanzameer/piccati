922b025bf5e11c863827b37aca83a60be7710ab6
Salman Zameer <salman.railstech@gmail.com>
2016-09-02 23:17:25 +0500

    replaced LIKE with ILIKE

--- app/api/v1/customer/information.rb
+++ app/api/v1/customer/information.rb
@@ -128,9 +128,9 @@ module Customer
       unless @requester.present?
         throw :error, status: 404, message: "Requester not found!"
       end
-
+      
       search = params[:search_string].downcase
-      @photographers = Photographer.where("(firstname LIKE ? OR lastname LIKE ?) AND confirmed_at IS NOT NULL", "%#{search}%", "%#{search}%")
+      @photographers = Photographer.where("(firstname ILIKE ? OR lastname ILIKE ?)", "%#{search}%", "%#{search}%")
     end
 
     desc "Get images with likes greater than 1000 (collection feed)"
