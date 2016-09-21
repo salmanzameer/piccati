5c0e4f559b43028459b7bff9a3bb2d20ebd78aee
Muhammmad Waqas <waqas@protonlabs.com>
2016-09-21 16:25:35 +0500

    Admin Dashboard styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910114842) do
+ActiveRecord::Schema.define(version: 20160919131819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -241,18 +241,18 @@ ActiveRecord::Schema.define(version: 20160910114842) do
     t.string   "lastname"
     t.string   "contnumber"
     t.string   "username"
-    t.string   "email",                       default: "", null: false
-    t.string   "encrypted_password",          default: "", null: false
+    t.string   "email",                       default: "",    null: false
+    t.string   "encrypted_password",          default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",               default: 0,  null: false
+    t.integer  "sign_in_count",               default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.integer  "client_id"
     t.integer  "event_id"
     t.string   "avatar_file_name"
@@ -262,7 +262,7 @@ ActiveRecord::Schema.define(version: 20160910114842) do
     t.string   "provider"
     t.string   "uid"
     t.integer  "package_id"
-    t.integer  "memory_consumed",             default: 0,  null: false
+    t.integer  "memory_consumed",             default: 0,     null: false
     t.string   "website"
     t.string   "authentication_token"
     t.string   "watermark_logo_file_name"
@@ -288,6 +288,7 @@ ActiveRecord::Schema.define(version: 20160910114842) do
     t.integer  "used_connects",               default: 0
     t.boolean  "terms_and_condition"
     t.string   "slug"
+    t.boolean  "is_approved",                 default: false
   end
 
   add_index "photographers", ["confirmation_token"], name: "index_photographers_on_confirmation_token", unique: true, using: :btree
